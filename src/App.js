cb5a977cec4a0af7efbbe565704921231940fc73
junhyuk <junhyuk0801@gmail.com>
2022-06-04 06:29:05 +0900

    Implement drive list page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+    BrowserRouter as Router,
+    Route,
+    Routes,
+    Navigate,
+} from "react-router-dom";
 
-import Layout from "./components/layout/layout";
 import FileManager from "./components/filemanager/fileManager";
 import AuthForm from "./components/auth/main";
 import ShareDrive from "./components/share/main";
+import DriveList from "./components/driveList/driveList";
 
 import "./App.css";
 
@@ -11,14 +16,15 @@ function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/:driveId" element={
-                    <Layout>
-                        <FileManager />
-                    </Layout>
-                }/>
-                <Route path="/login" element={<AuthForm type={"LOGIN"}/>} />
-                <Route path="/register" element={<AuthForm type={"REGISTER"}/>} />
-                <Route path="/share/:id" element={<ShareDrive/>} />
+                <Route path="/drive/:driveId" element={<FileManager />} />
+                <Route path="/drives" element={<DriveList />} />
+                <Route path="/login" element={<AuthForm type={"LOGIN"} />} />
+                <Route
+                    path="/register"
+                    element={<AuthForm type={"REGISTER"} />}
+                />
+                <Route path="/share/:id" element={<ShareDrive />} />
+                <Route path="/" element={<Navigate to="/drive/asd" />}></Route>
             </Routes>
         </Router>
     );
